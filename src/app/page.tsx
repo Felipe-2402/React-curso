ea6cda82f232b41df9728ef69007913aa62d449f
Felipe-2402 <felipe0000bm@gmail.com>
2026-02-20 00:22:19 -0300

    Aplicação das janelas e interações da janela da esquerda

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,14 @@ import CenterContent from "../components/CenterContent";
 
 
 export default function page () {
+  const leftNews = [
+    { title: "Novo limite de MEI atualizado", date:"2026", preview: "O governo discute aumento do teto anual do MEI para acompanhar a inflação."},
+    { title: "Mudanças na DAS mensal", date:"janeiro", preview: "A guia mensal terá novo valor baseado no salário mínimo atual."},
+    { title: "Nova categoria incluída", date:"Receita federal", preview: "Profissões digitais passam a poder aderir ao regime MEI."}
+  ];
   return (
   <main className="layout">
-    <LeftPanel />
+    <LeftPanel  items={leftNews} />
     <CenterContent />
     <RightPanel />
 
--- src/components/LeftPanel.tsx
+++ src/components/LeftPanel.tsx
@@ -1,8 +1,34 @@
-export default function LeftPanel() {
+"use client";
+import { useState } from "react";
+
+type News = {
+    title: string;
+    date: string;
+    preview: string;
+};
+
+export default function LeftPanel({items }: { items: News[] }) {
+    const [hovered, setHovered] = useState<number | null>(null);
+
     return (
         <aside className="left">
-            <h2>Painel</h2>
-            <p>Notícias ou alertas aparecerão aqui.</p>
+            <h2>Notícias</h2>
+            
+            {items.map((news, index) => (
+                <div 
+                key={index} 
+                className="news"
+                onMouseEnter={() => setHovered(index)}
+                onMouseLeave={() => setHovered(null)}
+                >
+                    <strong>{news.title}</strong>
+                    <p>{news.date}</p>
+
+                    {hovered === index && (
+                        <span className="preview">{news.preview}</span>
+                    )}
+                </div>
+            ))}
         </aside>
     );
 }
